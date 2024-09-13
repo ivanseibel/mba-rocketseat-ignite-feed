803d241803e6ea53451c0a3b765c969169384889
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-09-13 17:41:28 +0100

    Refactor Post component to include author information and styling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,53 @@
-export function Post({author, content}) {
+import styles from './Post.module.css';
+
+export function Post() {
   return (
-    <article>
-      <h2>{author}</h2>
-      <p>{content}</p>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img 
+            src="https://github.com/ivanseibel.png" 
+            alt="Ivan Seibel"
+            className={styles.avatar} 
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>
+              Ivan Seibel
+            </strong>
+            <span>
+              Software Engineer
+            </span>
+          </div>
+        </div>
+
+        <time 
+          dateTime="2021-03-25T12:00:00"
+          title="March 25, 2021 at 12:00 PM"
+        >
+          1 hour ago
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>
+          Hey, guys! 👋
+        </p>
+        
+        <p>
+          Today I&rsquo;m starting a new project from scratch. I&rsquo;m going to build a blog using ReactJS and Next.js. I&rsquo;m going to share the entire process with you.
+        </p>
+        
+        <p>
+          Stay tuned 👉 <a href='#'>github.com/ivanseibel/blogx</a>
+        </p>
+
+        <p>
+          <a>#reactjs</a> {' '}
+          <a>#nextjs</a> {' '}
+          <a>#webdevelopment</a>
+        </p>
+      </div>
     </article>
   )
 }
