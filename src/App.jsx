ca7e7d0afd90d378ddfe2bc6350479e3d3fe2b41
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-09-13 11:12:27 +0100

    Refactor App component to include Header, Sidebar, and Post components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import { Header } from "./components/Header"
+import { Post } from "./components/Post"
+
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Ivan Seibel"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec nunc nec nunc."
+          />
+          <Post 
+            author="Ivan Seibel"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec nunc nec nunc."
+          />
+          <Post 
+            author="Ivan Seibel"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec nunc nec nunc."
+          />
+        </main>
+      </div>
+    </div>
   )
 }
 
