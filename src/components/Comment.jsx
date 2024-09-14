db4f7a676c913a59e198c8f856e90e8733aac993
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-09-14 19:43:44 +0100

    Refactor Comment component to include content and applaud functionality

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,18 @@
+import React from 'react'
+
 import { ThumbsUp, Trash } from 'phosphor-react'
-import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment() {
+import styles from './Comment.module.css'
+
+export function Comment({ content, onDeleteComment }) {
+  const [applauses, setApplauses] = React.useState(0)
+
+  function handleApplaud() {
+    event.preventDefault()
+    setApplauses(applauses + 1)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar
@@ -23,17 +33,22 @@ export function Comment() {
               </time>
             </div>
 
-            <button title='Delete'>
+            <button 
+              title='Delete'
+              onClick={onDeleteComment}
+            >
               <Trash size={24} />
             </button>
           </header>
           <p>
-            Very nice! I&apos;m looking forward to seeing the final result. 😄
+            {content}
           </p>
         </div>
         <footer>
-          <button>
-            <ThumbsUp size={20} /> Applaud <span>20</span>
+          <button
+            onClick={handleApplaud}
+          >
+            <ThumbsUp size={20} /> Applaud <span>{applauses}</span>
           </button>
         </footer>
       </div>
