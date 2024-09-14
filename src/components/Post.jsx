bcba29b219cb55b97a3e3aa6dc7c9437fbc1beea
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-09-14 19:43:50 +0100

    Refactor Post component to include dynamic comments and comment submission functionality

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,75 +1,135 @@
+import React from 'react';
+import { format, formatDistanceToNow } from 'date-fns';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post(props) {
+  const [comments, setComments] = React.useState([]);
+  const [comment, setComment] = React.useState('');
+
+  const { avatar_url, name, role } = props.author;
+  const { publishedAt, content } = props.post;
+
+  const dateTitle = format(publishedAt, 'd MMM yyyy, HH:mm');
+  const distanceToNow = formatDistanceToNow(publishedAt, { addSuffix: true });
+
+  function handleCommentSubmit(event) {
+    event.preventDefault();
+
+    setComments([...comments, comment]);
+    setComment('');
+
+    event.target.reset();
+  }
+
+  function handleDeleteComment(index) {
+    const newComments = [...comments];
+    newComments.splice(index, 1);
+
+    setComments(newComments);
+  }
+
+  const isCommentEmpty = comment.trim().length === 0;
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <Avatar
-            src="https://github.com/ivanseibel.png"
-            alt="User Avatar"
+            src={avatar_url}
+            alt={name}
             hasBorder={true}
           />
 
           <div className={styles.authorInfo}>
             <strong>
-              Ivan Seibel
+              {name}
             </strong>
             <span>
-              Software Engineer
+              {role}
             </span>
           </div>
         </div>
 
         <time 
-          dateTime="2021-03-25T12:00:00"
-          title="March 25, 2021 at 12:00 PM"
+          dateTime={publishedAt.toISOString()}
+          title={dateTitle}
         >
-          1 hour ago
+          {distanceToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>
-          Hey, guys! 👋
-        </p>
-        
-        <p>
-          Today I&rsquo;m starting a new project from scratch. I&rsquo;m going to build a blog using ReactJS and Next.js. I&rsquo;m going to share the entire process with you.
-        </p>
-        
-        <p>
-          Stay tuned 👉 <a href='#'>github.com/ivanseibel/blogx</a>
-        </p>
+        {content.map((item, index) => (
+          <React.Fragment key={index}>
+            {item.type === 'paragraph' && (
+              <p>
+                {item.content}
+              </p>
+            )}
 
-        <p>
-          <a>#reactjs</a> {' '}
-          <a>#nextjs</a> {' '}
-          <a>#webdevelopment</a>
-        </p>
+            {item.type === 'text' && (
+              <span>
+                {item.content}
+              </span>
+            )}
+
+            {item.type === 'link' && (
+              <a 
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.content}
+              </a>
+            )}
+
+            {item.type === 'tags' && (
+              item.content.map((tag, index) => (
+                <a 
+                  key={index}
+                  href={`https://www.google.com/search?q=${String(tag).replace('#', '')}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {`${tag} `}
+                </a>
+              ))
+            )}
+          </React.Fragment>
+        ))}
+
       </div>
 
-      <form className={styles.commentForm}>
+      <form className={styles.commentForm} onSubmit={handleCommentSubmit}>
         <strong>
           Leave a comment
         </strong>
 
         <textarea
           placeholder="What do you think?"
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+          required
+          onInvalid={(event) => event.target.setCustomValidity('Please, leave a comment.')}
         ></textarea>
         
         <footer>
-          <button type="submit">
+          <button 
+            type="submit" 
+            disabled={isCommentEmpty}
+          >
             Comment
           </button>
         </footer>
 
         <div className={styles.commentList}>
-          <Comment />
-          <Comment />
-          <Comment />
+          {comments.map((comment, index) => (
+            <Comment key={index} content={comment} onDeleteComment={() => handleDeleteComment(index)} />
+          ))}
         </div>
       </form>
     </article>
