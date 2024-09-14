7b7ff1aac5742d65ae06b183b44c7225cd9ad4b1
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-09-14 12:51:41 +0100

    Refactor Sidebar component to use Avatar component for user avatars

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -11,10 +12,10 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar}
-          src="https://github.com/ivanseibel.png" 
-          alt="Ivan Seibel" 
+        <Avatar 
+          src="https://github.com/ivanseibel.png"
+          alt="Ivan Seibel"
+          hasBorder={true}
         />
 
         <strong>
